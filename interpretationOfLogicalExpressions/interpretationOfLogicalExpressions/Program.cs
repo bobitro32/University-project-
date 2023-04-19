7dd27afd2ce37642c61f0bf2d71f5b90ba1e313e
BobiDimitrova <114482027+BobiDimitrova@users.noreply.github.com>
2023-04-19 17:50:05 +0300

    It is clearer the code
    Here i wrote the Split method but it can only split by char for now.

--- interpretationOfLogicalExpressions/interpretationOfLogicalExpressions/Program.cs
+++ interpretationOfLogicalExpressions/interpretationOfLogicalExpressions/Program.cs
@@ -13,10 +13,11 @@ namespace HelloWorld
 
             String input = Console.ReadLine();
             checkCommand(input);
+            String[] afterSplit = SplitMethod(' ',input);
+            foreach (String a in afterSplit) {
+                Console.WriteLine(a);
+            }
             
-            
-
-
         }
         public static void checkCommand(String input) {
             String command = "";
@@ -25,34 +26,20 @@ namespace HelloWorld
                     break; 
                 }
                 command += symbol;
-
-            
             }
 
             switch (command) {
                 case "DEFINE":
-                   
                     defineFunc(input);
                     break;
                 case "SOLVE":
-                    
                     break;
                 case "ALL":
-                    
                     break;
                 case "FIND":
-                    
                     break;
-
             }
 
-            
-            
-           
-
-
-            
-        
         }
 
         public static void defineFunc(String input)
@@ -63,36 +50,56 @@ namespace HelloWorld
 
             string[] functionZandCondition = new string[2];
             string currentExpresstion = "";
-          
 
             for (int startingSymbol = "DEFINE".Length +1; startingSymbol < input.Length; startingSymbol++)
 
             {
-                
                 // If the currentSymbol == : -> I add the string to the array and after that refresh the string and continue to the next command
                 if (input[startingSymbol] == ':') {
                     functionZandCondition[0] = currentExpresstion;
-                  
                     currentExpresstion = "";
                     continue;
-
                 }
-
                 //here i check if the current symbol is  diffrent from a quotation mark or space and if it is then i add it to the string ( that's how i build the string)
                 if (input[startingSymbol] != ' '  && input[startingSymbol] != '"') {
-                    currentExpresstion += input[startingSymbol];
-              
+                    currentExpresstion += input[startingSymbol];  
                 }
                 // here i add the last string _> the expression so after that i know the expression is the second string in the array, so i can access it easier to the tree
                 if (startingSymbol == input.Length - 1) {
                     functionZandCondition[1] = currentExpresstion;
                     break;
-                
                 }
             }
 
-           
+        }
 
+
+        public static string[] SplitMethod(char separator,string text ) {
+            int count = 1;
+            foreach (char symbol in text) {
+                if (symbol == separator) { 
+                    count++;
+                }
+            }
+            string[] symbols = new string[count];
+            String currentValue = "";
+            foreach (char symbol in text)
+            {
+      
+                if (symbol == separator)
+                {
+                    symbols[symbols.Length - count] = currentValue;
+                    count--;
+                    currentValue = "";
+                }
+                else {
+                    currentValue += symbol;        
+
+                }
+            }
+            return symbols;
+
+        
         }
 
 
